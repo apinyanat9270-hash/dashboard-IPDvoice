eb1c9f6871559b876fe8b1256bea6f8dc139dc18
apinyanat9270-hash <apinya.nat9270@gmail.com>
2026-05-06 20:14:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ df = pd.read_csv(url)
 col1, col2, col3 = st.columns([1,2,1])
 
 with col2:
-    st.image("S__13254690_0.jpg", width=150)
+    st.image("S__13254690_0.jpg", width=140)
 
 st.markdown("""
-<h1 style='text-align: center; color: #2c3e50;'>
+<h1 style='text-align: center;'>
 📊 ประเมินความพึงพอใจผู้ป่วยใน<br>โรงพยาบาลฝาง
 </h1>
 """, unsafe_allow_html=True)
